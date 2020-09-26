0932604640c81729440289df1b6381b06fa1fc93
budourush <g.ff.k.96@gmail.com>
2020-09-26 22:57:29 +0900

    DBをpostgresqlへ変更

--- helloworldproject/helloworldproject/settings.py
+++ helloworldproject/helloworldproject/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'helloworldproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '192.168.56.100',
+        'PORT': '5432',
     }
 }
 
