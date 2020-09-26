8117d40032f0cdfe753d0e8ab468b3ca9f144cf4
budourush <g.ff.k.96@gmail.com>
2020-09-27 00:17:11 +0900

    モデル作成

--- helloworldproject/blogpost/models.py
+++ helloworldproject/blogpost/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class SampleModel(models.Model):
+    title = models.CharField(max_length=100)
+    number = models.IntegerField()
